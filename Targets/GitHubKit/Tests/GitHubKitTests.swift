b7f7ad5196a053ca630aa1fbe77317237632d521
Jaemyeong Jin <jaemyeong@me.com>
2023-03-28 10:26:16 +0900

    Initial commit
    Signed-off-by: Jaemyeong Jin <jaemyeong@me.com>

---
+++ Targets/GitHubKit/Tests/GitHubKitTests.swift
@@ -0,0 +1,8 @@
+import Foundation
+import XCTest
+
+final class GitHubKitTests: XCTestCase {
+    func test_example() {
+        XCTAssertEqual("GitHubKit", "GitHubKit")
+    }
+}
---
+++ Targets/GitHubUI/Tests/GitHubUITests.swift
@@ -0,0 +1,8 @@
+import Foundation
+import XCTest
+
+final class GitHubUITests: XCTestCase {
+    func test_example() {
+        XCTAssertEqual("GitHubUI", "GitHubUI")
+    }
+}
